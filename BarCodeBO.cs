84d2acceb17761a8aea4be1ab6e78b00f4fca739
ejw2405 <e.woerlee@verdouw.nu>
2020-06-10 17:22:46 +0200

    First Rev

---
+++ BarCodeBO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class BarCodeBO
+    {
+        private readonly ConcurrentDictionary<string, BarCodePrinterHandler> barCodePrintHandlers =
+            new ConcurrentDictionary<string, BarCodePrinterHandler>();
+
+
+        public BarCodeBO()
+        {
+        }
+
+        public void Print(string ipaddress, string zpl)
+        {
+            try
+            {
+                BarCodePrinterHandler barCodePrinterHandler =
+                        barCodePrintHandlers.GetOrAdd(ipaddress, new BarCodePrinterHandler(ipaddress));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+    }
+}
---
+++ BarCodePrinterHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class BarCodePrinterHandler
+    {
+        const int boundedQueueSize = 100;   // max aantal labels die (tegelijkertijd) kunnen worden verwerkt per printer
+        private readonly string ipAddress; // welke printer (ip)
+
+        private Channel<String> printerChannel = Channel.CreateBounded<string>(boundedQueueSize);
+
+        public BarCodePrinterHandler(string ipAddress)
+        {
+            this.ipAddress = ipAddress;
+            Task.Run(ConsumePrintOpdracht);
+        }
+
+        public async Task ProducePrintOpdracht(string zpl)
+        {
+            await printerChannel.Writer.WriteAsync(zpl);
+        }
+
+        public async Task ConsumePrintOpdracht()
+        {
+            while (await printerChannel.Reader.WaitToReadAsync())
+            {
+                // in het geval van meerdere consumers zou een item toch al weg kunnen zijn.
+                while (printerChannel.Reader.TryRead(out string zpl))
+                {
+                    // Debug.WriteLine($"printing document to zebraprinter: {zpl}");
+                    // await PrintLabelToZebraPrinter(document);
+                }
+            }
+        }
+
+        public async Task PrintLabelToZebraPrinter(string zpl)
+        {
+            return;
+        }
+
+    }
+}
